b995d745d09c81058edfee2958f45180ceabe1a3
dejviud98 <davidsucur111@gmail.com>
2020-05-04 17:52:55 +0200

    new queries

--- createTables.php
+++ createTables.php
@@ -12,20 +12,28 @@ $pdo = new PDO('mysql:host=localhost;dbname=php1','root');
 //     $pdo->query("CREATE TABLE testerino(
 //         id int primary key,
 //         name varchar(30) not null)");
-//     // $pdo->query("CREATE TABLE roba (
-//     //     roba_id int primary key auto_increment,
-//     //     magacin_id int not null,
-//     //     naziv VARCHAR(30) NOT NULL,
-//     //     lokacija VARCHAR(30) NOT NULL,
-//     //     FOREIGN KEY (`magacin_id`) REFERENCES `magacin` (`id`)
-//     // )");
+    // $pdo->query("CREATE TABLE roba (
+    //     roba_id int primary key auto_increment,
+    //     magacin_id int not null,
+    //     naziv VARCHAR(30) NOT NULL,
+    //     kolicina int NOT NULL,
+    //     FOREIGN KEY (`magacin_id`) REFERENCES `magacin` (`id`)
+    // )");
 // }catch(PDOException $e){
 //     print $e;
 // }
 
 
-$pdo->query("CREATE TABLE magacin (
-id int primary key auto_increment,
-naziv VARCHAR(30) NOT NULL,
-lokacija VARCHAR(30) NOT NULL
-)");
+// $pdo->query("CREATE TABLE magacin (
+// id int primary key auto_increment,
+// naziv VARCHAR(30) NOT NULL,
+// lokacija VARCHAR(30) NOT NULL
+// )");
+
+    $pdo->query("CREATE TABLE roba (
+        roba_id int primary key auto_increment,
+        magacin_id int not null,
+        naziv VARCHAR(30) NOT NULL,
+        kolicina int NOT NULL,
+        FOREIGN KEY (`magacin_id`) REFERENCES `magacin` (`id`)
+    )");
